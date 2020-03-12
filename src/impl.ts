6b8ba4e5dc3cab4cd14950cb23d740a193cb31c9
pokutuna <popopopopokutuna@gmail.com>
2020-03-13 04:46:41 +0900

    not async

--- src/impl.ts
+++ src/impl.ts
@@ -7,21 +7,21 @@ import {
 
 import {Request, Response} from 'express';
 
-export async function makeFunction(options: ExportOptions & LoadOptions) {
+export function makeFunction(options: ExportOptions & LoadOptions) {
   return async (req: Request, res: Response) => {
     const action = req.body.action;
 
     if (action === 'export') {
       await requestExportFirestore(options);
-      return res.status(204);
+      return res.status(200).send('export requested');
     }
 
     if (action === 'load') {
       await requestLoadToBigQuery(options);
-      return res.status(204);
+      return res.status(200).send('load requested');
     }
 
     console.warn('action not found');
-    return res.status(400);
+    return res.status(400).send('action not found');
   };
 }
